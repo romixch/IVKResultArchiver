e67139204eec76660380d59b161e6cf869e6278b
Roman Schaller <roman.schaller@gmail.com>
2014-08-05 22:16:52 +0200

    make base path configurable

--- src/main/java/ch/romix/ivk/resultarchiver/RESTURI.java
+++ src/main/java/ch/romix/ivk/resultarchiver/RESTURI.java
@@ -2,16 +2,20 @@ package ch.romix.ivk.resultarchiver;
 
 public class RESTURI {
 
-  // private static final String BASE = "http://korbball.turnverband.ch/ws/";
-  private static final String BASE = "http://localhost/www.turnverband.ch/korbball/ws/";
+  private static final String BASE = "http://korbball.turnverband.ch/ws/";
   private static final String GROUPS = "groupsV1.php";
   private static final String TABLE = "tableV2.php?group=%s";
 
   public static String getGroupURI() {
-    return BASE + GROUPS;
+    return appendToBase(GROUPS);
   }
 
   public static String getTableURI(int id) {
-    return BASE + String.format(TABLE, id);
+    return appendToBase(String.format(TABLE, id));
+  }
+
+  private static String appendToBase(String path) {
+    String base = System.getProperty("BASE_PATH", BASE);
+    return base + path;
   }
 }
